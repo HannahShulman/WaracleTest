42fa2c9342efaa52f700a2f26df293f01ef3249d
Hanna Shulman <hanlbb91@gmail.com>
2022-10-27 16:18:28 +0100

    Create and add dto and entity to project

---
+++ app/src/main/java/com/hanna/waracle/test/domain/models/dto/model/CakeItemDto.kt
@@ -0,0 +1,3 @@
+package com.hanna.waracle.test.domain.models.dto.model
+
+data class CakeItemDto(val title: String, val desc: String, val image: String)
---
+++ app/src/main/java/com/hanna/waracle/test/domain/models/entities/CakeItem.kt
@@ -0,0 +1,3 @@
+package com.hanna.waracle.test.domain.models.entities
+
+data class CakeItem(val title: String, val description: String, val url: String)
